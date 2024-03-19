86f7a85ae4850673fca39ffc6bc67d4af9399e4d
Yahya Ansari <148518803+Yahya-7860@users.noreply.github.com>
2024-03-19 20:06:24 +0530

    Add files via upload

---
+++ Basic React/src/App.js
@@ -0,0 +1,16 @@
+import ShaminBhai from "./shaminBhai";
+
+
+function App() {
+  return (
+
+    <>
+    <h1>Hello my name is shamin</h1>
+
+    <ShaminBhai/>
+    
+    </>
+  );
+}
+
+export default App;

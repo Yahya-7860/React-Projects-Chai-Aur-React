ae2b225c8a205e85482f3d6f637a0a89e0eea89d
Yahya Ansari <148518803+Yahya-7860@users.noreply.github.com>
2024-03-19 20:08:06 +0530

    Add files via upload

---
+++ Basic Vite React/src/App.jsx
@@ -0,0 +1,13 @@
+import Shamin from "./shamin";
+
+function App() {
+  return (
+    <>
+    <h1>i am shamin from vite</h1>
+    
+    <Shamin/>
+    </>
+  );
+}
+
+export default App;
---
+++ Basic Vite React/src/shamin.jsx
@@ -0,0 +1,8 @@
+//!this is only a compnent for testing purpose 
+
+
+export default function Shamin() {
+  return (
+    <h3>mera nam shamin hai or ye H3 tag hai</h3>
+  )
+}
